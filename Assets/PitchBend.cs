1c4ef9537a3d5082e095c29a79beee095500e482
paulatwarp <paul.sinnett@warpdigital.com>
2022-06-22 12:21:08 +0100

    implement with audio filter read

--- Assets/PitchBend.cs
+++ Assets/PitchBend.cs
@@ -2,20 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Oscillator
+{ 
+    public float frequency = 440.0f;
+    public float amplitude = 0.5f;
+    float time;
+
+    public Oscillator(float frequency, float amplitude)
+    {
+        this.frequency = frequency;
+        this.amplitude = amplitude;
+        time = 0;
+    }
+
+    public float Sample(float tick)
+    {
+        time = Mathf.Repeat(time + tick * frequency, 1f);
+        return Mathf.Sin(time * 2.0f * Mathf.PI) * amplitude;
+    }
+}
+
+
 public class PitchBend : MonoBehaviour
 {
-    public AudioSource source;
-    public float frequency = 1;
+    public int frequency = 10;
+    public float strength = 0.5f;
+    public float pitch = 440.0f;
     public float amplitude = 1;
-    float time;
+    float tick;
+    Oscillator tone;
+    Oscillator bend;
 
     private void Start()
     {
+        tick = 1.0f / AudioSettings.outputSampleRate;
+        tone = new Oscillator(pitch, amplitude);
+        bend = new Oscillator(frequency, strength);
     }
 
-    void Update()
+    void OnAudioFilterRead(float[] samples, int channels)
     {
-        time += Time.fixedDeltaTime * 2f * Mathf.PI * frequency;
-        source.pitch = 1.0f + Mathf.Sin(time) * amplitude;
+        int index = 0;
+        while (index < samples.Length)
+        {
+            tone.frequency = pitch * (1.0f + bend.Sample(tick));
+            float sample = tone.Sample(tick);
+            for (int channel = 0; channel < channels; ++channel)
+            {
+                samples[index + channel] = sample;
+            }
+            index += channels;
+        }
     }
 }
